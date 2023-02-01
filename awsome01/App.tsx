fafdac5ece12fd96f3495a127263098e1bfc6f0a
Fayaj Hossain <fayazhossain88@gmail.com>
2023-02-01 23:15:54 +0530

    added the component TextInput

--- awsome01/App.tsx
+++ awsome01/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, TextInput} from 'react-native';
 function App() {
   return (
     <SafeAreaView>
       <View>
+        <TextInput />
         <Text>Hello World!</Text>
         <Text>Hello World!</Text>
         <Text>Hello World!</Text>
